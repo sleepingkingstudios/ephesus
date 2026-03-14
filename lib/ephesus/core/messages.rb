3b8a776dffc6d2780396bd6dfcb0f92b7280b2e0
Rob Smith <sleepingkingstudios@gmail.com>
2026-03-14 17:05:27 -0400

    Implement Messages::Subscriber.

--- lib/ephesus/core/messages.rb
+++ lib/ephesus/core/messages.rb
@@ -7,6 +7,7 @@ module Ephesus::Core
   module Messages
     autoload :Definitions,  'ephesus/core/messages/definitions'
     autoload :Publisher,    'ephesus/core/messages/publisher'
+    autoload :Subscriber,   'ephesus/core/messages/subscriber'
     autoload :Subscription, 'ephesus/core/messages/subscription'
     autoload :Typing,       'ephesus/core/messages/typing'
   end
