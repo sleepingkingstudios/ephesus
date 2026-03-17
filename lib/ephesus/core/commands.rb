824c123c5e1ac4c1ec4cf72dd4e0f51dfa200165
Rob Smith <sleepingkingstudios@gmail.com>
2026-03-17 01:13:30 -0400

    Implement Commands::DisconnectActor.

--- lib/ephesus/core/commands.rb
+++ lib/ephesus/core/commands.rb
@@ -5,6 +5,7 @@ require 'ephesus/core'
 module Ephesus::Core
   # Namespace for command implementations.
   module Commands
-    autoload :ConnectActor, 'ephesus/core/commands/connect_actor'
+    autoload :ConnectActor,    'ephesus/core/commands/connect_actor'
+    autoload :DisconnectActor, 'ephesus/core/commands/disconnect_actor'
   end
 end
